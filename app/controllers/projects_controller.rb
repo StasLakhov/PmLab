ecf680bb385a040afd45fcd6c7429d389ae75a15
StasLakh <doctorbyron05@gmail.com>
2017-05-16 05:51:59 -0700

    add status

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -37,7 +37,7 @@ class ProjectsController < ApplicationController
 
   private
   def project_params
-    params.require(:project).permit(:name, :description)
+    params.require(:project).permit(:name, :description, :status)
   end
 
 end
