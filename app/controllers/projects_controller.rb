686333c9c24873e2f6a07a0e679702764ee09ed3
StasLakh <doctorbyron05@gmail.com>
2017-05-22 05:48:50 -0700

    change tables structure 2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -37,7 +37,7 @@ class ProjectsController < ApplicationController
 
   private
   def project_params
-    params.require(:project).permit(:name, :description, :status, :startdate, :enddate, :receive)
+    params.require(:project).permit(:name, :description, :status, :startdate, :enddate, :receive, :customer_id )
 
   end
 
